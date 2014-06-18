ce3e5afd34aff5218aacd27959f803f336df3d78
blairprophett <blaircuny@gmail.com>
2014-06-18 16:22:20 -0700

    tried to create instance variables for results, not quite working, commented out

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,12 +4,24 @@ class SitesController < ApplicationController
     params = {
            limit: 3,
            category_filter: 'dog_parks',
-           # term: params[:term]
+           #term: params[:term],
          }
-    @request = (Yelp.client.search('San Francisco', params).to_json)
+    @request = Yelp.client.search('San Francisco', params).to_json
    
     @results = JSON.parse(@request)
-    # binding.pry
+
+    @yelp_info = @results['businesses']
+
+    # @yelp_info.each do |business|
+    #   @name = business['name']
+    #   @img = business['image_url']
+    #   @neighborhood = business['location']['neighborhoods'][0]
+    #   @address = business['location']['address'][0]
+    #   @zip = business['location']['display_address'][2]
+    #   @yelp_url = business['url']
+    # end
+
   end
 
 end
+
