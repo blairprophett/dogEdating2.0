30d0b2a27e37afbab4cff74733239788a9019d27
blairprophett <blaircuny@gmail.com>
2014-06-19 11:48:57 -0700

    dog show method in controller and views

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -9,6 +9,11 @@ class DogsController < ApplicationController
     redirect_to(dog)
   end
 
+  def show
+    @dog = Dog.find_by_id(params[:id])
+  end
+
+
   private
     def dog_params
       params.require(:dog).permit(:name, :breed, :age, :image, :park_id)
