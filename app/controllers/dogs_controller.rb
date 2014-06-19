1682038ad9690e1edb2eca687d795600e9995282
blairprophett <blaircuny@gmail.com>
2014-06-19 11:33:08 -0700

    new and create methods for dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,16 @@
 class DogsController < ApplicationController
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    dog = Dog.create dog_params
+    redirect_to(dog)
+  end
+
+  private
+    def dog_params
+      params.require(:dog).permit(:name, :breed, :age, :image, :park_id)
+    end
 end
