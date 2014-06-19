cf7392496e1b60acc3e5389c0de2a0a11a7e9e9c
blairprophett <blaircuny@gmail.com>
2014-06-18 21:08:21 -0700

    searching by lat/long

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -6,14 +6,18 @@ class SitesController < ApplicationController
            category_filter: 'dog_parks',
            #term: params[:term],
          }
-    @request = Yelp.client.search('San Francisco', params).to_json
+
+    coordinates = { latitude: 37.7577, longitude: -122.4376 }
+
+    @request = Yelp.client.search_by_coordinates(coordinates, params).to_json
    
     @results = JSON.parse(@request)
 
     @yelp_info = @results['businesses']
 
     # @yelp_info.each do |business|
-    #   @name = business['name']
+       #@name = @yelp_info[0]
+       #binding.pry
     #   @img = business['image_url']
     #   @neighborhood = business['location']['neighborhoods'][0]
     #   @address = business['location']['address'][0]
