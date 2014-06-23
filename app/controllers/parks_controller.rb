fda0fa596e89a79583c601ea6aacec268200cfa3
blairprophett <blaircuny@gmail.com>
2014-06-22 22:45:38 -0700

    added controller methods for dogs and parks

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -25,6 +25,50 @@ class ParksController < ApplicationController
     #   @yelp_url = business['url']
    # end
   end
+  
+ def index
+    @parks = Park.all
+  end
+
+  def new
+    @park = Park.new
+  end
+
+  def create
+    @park = Park.create park_params
+    redirect_to(park)
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    @dogs = @park.dogs
+  end
+
+  def edit
+    @park = Park.find(params[:id])
+  end
+
+  def update
+    @park = Park.find(params[:id])
+      if @park.update_attributes(park_params)
+        redirect_to(@park)
+      else
+        render :edit
+      end
+    end
+
+  def search
+    @parks = Park.all
+    @search = SimpleSearch.new SimpleSearch.get_params(params)
+    if @search.valid?
+      @parks = @search.search_within Park.all, :name
+    end
+  end
+
+  private
+    def park_params
+      params.require(:park).permit(:name, :address, :leash_rule, :image)
+    end
 
 
 end
