d603fb7a98442e780d971868098eefbb1c57d742
blairprophett <blaircuny@gmail.com>
2014-06-23 22:47:24 -0700

    added unique park identifyer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623183357) do
+ActiveRecord::Schema.define(version: 20140624010932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20140623183357) do
     t.float    "latitude"
     t.float    "longitude"
     t.text     "address"
+    t.string   "yelp_id"
   end
 
   create_table "receipts", force: true do |t|
