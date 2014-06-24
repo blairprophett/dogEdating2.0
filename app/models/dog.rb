263c1fcea3814043c466e5ad4bcb8ddfc0888fe8
blairprophett <blaircuny@gmail.com>
2014-06-23 22:49:49 -0700

    unique park identiifyer saving, but also throwing error, added model associations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,6 @@
 class Dog < ActiveRecord::Base
   belongs_to :user
-  belongs_to :parks
+  belongs_to :park
 
   #for CarrierWave image uploader
   mount_uploader :image, ImageUploader
--- app/models/park.rb
+++ app/models/park.rb
@@ -1,6 +1,6 @@
 class Park < ActiveRecord::Base
-
   has_many :dogs
+  
   #geocoder gem- automatically fetch coordinates after validation
   #stored in latitude and longitude attributes
   geocoded_by :address
